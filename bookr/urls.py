865d50f4dae98739f19f47b9d4895cb668371c56
Your Name <alba.shaykhullina@gmail.com>
2023-12-05 20:18:02 +0300

    5 sprint

--- bookr/urls.py
+++ bookr/urls.py
@@ -1,7 +1,7 @@
 """bookr URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/dev/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,12 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import reviews.views
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('book-search/', reviews.views.book_search),
-    path('', include('reviews.urls')),
+    path('', include('reviews.urls'))
 ]
